ef243fb9fcece6eb7b0a084f97a9076eeabc2f7c
John Hurst <john.b.hurst@gmail.com>
2014-04-09 14:27:29 +1200

    First working examples.

---
+++ src/test/java/nz/co/skepticalhumorist/java8/StreamsExamples.java
@@ -0,0 +1,32 @@
+// Copyright 2014 Skeptical Humorist
+// John Hurst (john.b.hurst@gmail.com)
+// 2014-04-08
+
+package nz.co.skepticalhumorist.java8;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamsExamples {
+
+  @Test
+  public void testOk() {
+    List<Integer> v = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  }
+
+  @Test
+  public void cannotReuseStreams() {
+    Stream<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).stream();
+    Predicate<Integer> isEven = v -> (v & 1) == 0;
+    Predicate<Integer> isOdd = v -> (v & 1) == 1;
+    List<Integer> evens = ints.filter(isEven).collect(Collectors.toList());
+    List<Integer> odds = ints.filter(isOdd).collect(Collectors.toList());
+  }
+
+
+}
